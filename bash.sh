2de3959d32c8235a6558f2e5169efc54c7c1edb8
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-27 04:09:42 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,13 @@
 git clone https://github.com/Web4application/kubuverse.git
 cd kubuverse
 docker-compose up --build
+
+git clone https://github.com/Web4application/script_analyzer_bot.git
+cd script_analyzer_bot
+pip install -r requirements.txt
+
+
+## Running the API
+
+uvicorn app.main:app --reload
+http://127.0.0.1:8000/docs
